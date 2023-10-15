efe7a461e753a530e2d71b99ad1e6e5fbfffee4b
shadowX12 <routpratik09@gmail.com>
2023-10-15 14:04:14 +0530

    major changes in homepage

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,20 +1,14 @@
-// import logo from './logo.svg';
-// import Button from '@mui/material/Button'
-import '../Styles/App.css'
 
-// import Loginpage from './Loginpage';
-// import Headerbar from './Headerbar'
-// import Navbar from './Navbar';
-// import Sidebar from './Sidebar';
-import Homepage from './Homepage';
-function App() { return(<Homepage/>);}
-// import Searchelement from './Searchelement';
-// function App() { return(<Navbar/>);}
-
-// function App() { return(<Headerbar/>);}
-// function App() { return(<Searchelement/>);}
-
-
+// import Button from "@mui/material/Button";
+import "../Styles/App.css";
+import Navbar from "./Navbar";
+import Homepage from "./Homepage";
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Homepage />
+    </div>
+  );
+}
 export default App;
-
-
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -11,11 +11,6 @@ import AddIcon from '@mui/icons-material/Add';
 import ForumIcon from '@mui/icons-material/Forum';
 import { NotificationsActive } from "@mui/icons-material";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import FeedbackIcon from '@mui/icons-material/Feedback';
-import SettingsIcon from '@mui/icons-material/Settings';
-import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 
 function Navbar() {
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
@@ -24,24 +19,20 @@ function Navbar() {
     const toggleDropdown = () => {
         setIsDropdownOpen(!isDropdownOpen);
     };
-
-    // Define a function to close the dropdown
-    const closeDropdown = () => {
-        setIsDropdownOpen(false);
-    };
-
-    const logout = () => {
-        // Implement logout logic here
-    };
-
     return (
         <div className="header">
             {/* Header is split into 3 areas: center, left & right */}
             <div className="header_left">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png" alt="" />
                 <div className="header_input">
-                    <SearchIcon />
-                    <input className="search_input" placeholder="Search Facebook" type="text" />
+                  <SearchIcon />
+                    <input
+                       className="search_input"
+                       placeholder="Search Facebook"
+                       type="text"
+                    //    value={searchQuery}
+                    //    onChange={handleInputChange}
+                   />
                 </div>
             </div>
             <div className="header_center">
@@ -75,49 +66,7 @@ function Navbar() {
                 <IconButton onClick={toggleDropdown}>
                     <ExpandMoreIcon /> 
                 </IconButton>
-                {isDropdownOpen && 
-                    <div className="dropdown-container">
-                        <div className={`dropdown-trigger ${isDropdownOpen ? 'open' : ''}`} onClick={toggleDropdown}>
-                            <i className="dropdownIcon"></i>
-                        </div>
-                        {isDropdownOpen && (
-                            <div className="dropdown-content" onBlur={closeDropdown} tabIndex={0}>
-                                <div className="dropdown-item">
-                                    <Avatar src="https://i.ibb.co/1zmBtwr/84241059-189132118950875-4138507100605120512-n.jpg" alt="User Avatar" />
-                                    <div className="dropdown-text">
-                                        <h1>Your Name</h1>
-                                        <p>See your profile</p>
-                                    </div>
-                                </div>
-                                <div className="hr" />
-                                <div className="dropdown-item">
-                                    <FeedbackIcon />
-                                    <div className="dropdown-text">
-                                        <h1>Give Feedback</h1>
-                                        <p>Help us improve</p>
-                                    </div>
-                                </div>
-                                <div className="hr" />
-                                <div className="dropdown-item">
-                                    <SettingsIcon />
-                                    <h1>Settings & Privacy</h1>
-                                </div>
-                                <div className="dropdown-item">
-                                    <HelpOutlineIcon />
-                                    <h1>Help & Support</h1>
-                                </div>
-                                <div className="dropdown-item">
-                                    <Brightness4Icon />
-                                    <h1>Dark Mode</h1>
-                                </div>
-                                <div className="dropdown-item" onClick={logout}>
-                                    <ExitToAppIcon />
-                                    <h1>Log out</h1>
-                                </div>
-                            </div>
-                        )}
-                    </div>
-                }
+                
             </div>
         </div>
     );
