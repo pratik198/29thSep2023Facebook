57127a2fee520620522be57331277e1098767fc3
shadowX12 <routpratik09@gmail.com>
2023-10-19 17:16:20 +0530

    jwt

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,18 +2,21 @@ import Loginpage from "./Loginpage";
 import "../Styles/App.css";
 import Navbar from "./Navbar";
 import Homepage from "./Homepage";
+import Updatepassword from "./Updatepassword";
+import CreatePage from "./CreatePage";
 
 import Signup from "./Signup";
 function App() {
   return (
     <div>
       {/* <Headerbar/> */}
-      <Loginpage/>
-      {/* <Navbar /> */}
-      {/* <Homepage />   */}
+      {/* <Loginpage/> */}
+      {/* <Navbar /> 
+      <Homepage />   */}
     
       {/* <Signup/> */}
-    
+      <CreatePage/>
+    {/* <Updatepassword/> */}
 
     </div>
   );
--- src/Components/Loginpage.js
+++ src/Components/Loginpage.js
@@ -1,94 +1,11 @@
-// import React, { useEffect,useState } from "react";
-// import "../Styles/Loginpage.css";
-// import Button from "@mui/material/Button";
-// function Loginpage() {
-//   const [emailId, setEmailId] = useState("");
-//   const [passowrd, setPassword] = useState("");
-//   const api = "https://academics.newtonschool.co/api/v1/user/login";
-//   const Login = async () => {
-//     const response = await fetch(api, {
-//       method: "POST",
-//       body: JSON.stringify({
-//         email: "pr@gmail.com",
-//         password: "pr",
-//         appType: "facebook",
-//       }),
-//       headers: {
-//         "Content-Type": "application/json",
-//         projectID: "f104bi07c490",
-//       },
-//     });
-//     const r = await response.json();
-//     console.log(r);
-//   };
-//   useEffect(() => {
-//     Login();
-//   }, []);
-//   return (
-//     <div className="container">
-//       {/* Left side  */}
-//       <div className="left-container">
-//         <div className="content">
-//           <div className="f-logo">
-//             <img
-//               src="https://static.xx.fbcdn.net/rsrc.php/yI/r/4aAhOWlwaXf.svg"
-//               alt="..."
-//               className="logo"
-//             ></img>
-//           </div>
-//           <div className="F-content">
-//             <p>
-//               Facebook helps you connect and share <br /> with the people in
-//               your life.
-//             </p>
-//           </div>
-//         </div>
-//       </div>
-//       {/* Right Side */}
-//       <div className="right-container">
-//         <div className="card-container">
-//           <div className="card-details">
-//             <div className="input-filed">
-//               <input
-//                 type="text"
-//                 name="text"
-//                 placeholder="Email address or phone number"
-//               ></input>
-//               <input
-//                 type="password"
-//                 name="password"
-//                 placeholder="Password"
-//               ></input>
-//             </div>
-//             <div className="login-button">
-//               <Button variant="contained" className="Button">
-//                 Log In
-//               </Button>
-//             </div>
-//             <div className="Forgot-text">
-//               <a href="#">Forgotten password?</a>
-//             </div>
-//             <div className="line"></div>
-
-//             <div className="create-button">
-//               <button type="button" className="C-A-Button">
-//                 Create new account
-//               </button>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Loginpage;
 
 import React, { useState } from "react";
 import "../Styles/Loginpage.css";
 import Button from "@mui/material/Button";
 import HomePage from './Homepage';
+// import Updatepassword from "./Updatepassword";
 // import { useNavigate } from "react-router-dom";
+import { getBearerToken, setBearerToken } from "./Datastore";
 
 function Loginpage() {
   const projectID = "f104bi07c490";
@@ -129,6 +46,8 @@ function Loginpage() {
       if (response.ok) {
         console.log("Successfully logged in");
         setIsLoggedIn(true);
+        let json = await response.json();
+        setBearerToken(json["token"]);
         // navigate("/HomePage");
       } else {
         const errorData = await response.json();
