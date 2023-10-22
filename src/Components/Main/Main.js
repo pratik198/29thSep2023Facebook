7f76a7eab2d607669b9b4f4b4d137a271c8c8c71
shadowX12 <routpratik09@gmail.com>
2023-10-22 18:39:17 +0530

    right side widget

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -4,10 +4,13 @@ import Homepage from "../Homepage";
 import '../Main/Main.css'
 import WhatIsOnUrMind from "../WhatIsOnUrMind";
 import SidebarLeft from "../SidebarLeft";
+import RightSideBar from "../RightSideBar";
+
 function Main() {
   return (
     <div>
       <Navbar />
+      <RightSideBar/>
       <WhatIsOnUrMind/>
       <Homepage />
       <SidebarLeft/>
