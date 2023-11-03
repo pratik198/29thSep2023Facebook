fb09d554890e0217730562baec9c0e42bf6e4c42
shadowX12 <routpratik09@gmail.com>
2023-11-03 20:34:58 +0530

    comments

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -16,136 +16,39 @@ import chat from "../Images/chat.png";
 import like2 from "../Images/like 2.png";
 import comment from "../Images/comment.png";
 import send from "../Images/send.png";
+
 function Homepage() {
   const [Data, setData] = useState([]);
-  const [isPostLiked, setPostLiked] = useState(false);
-  const [commentData, setCommentData] = useState([]);
-  const bearerToken = localStorage.getItem("token");
-  const [comments, setComments] = useState([]);
-  let api1 =
-    'https://academics.newtonschool.co/api/v1/facebook/post?search={"author.name":"Carmen Shanahan"}';
-  let api = "https://academics.newtonschool.co/api/v1/facebook/post";
-  useEffect(() => {
-    getComment();
-    // setCommentData();
-    GetData();
-    setPostLiked(false);
-  }, [isPostLiked]);
+  const [comments, setComments] = useState({});
 
-  const getComment = async (postId) => {
-    console.log("inside get comment");
-    console.log(postId);
-    const token = localStorage.getItem("token");
-    const token_ = "Bearer " + token;
-    console.log(token_);
-    const response = await fetch(
-      `https://academics.newtonschool.co/api/v1/facebook/post/${postId}/comments`,
-      {
-        method: "GET",
-        headers: {
-          Authorization: token_,
-          projectID: "f104bi07c490",
-        },
-      }
-    );
-    console.log("Anurag Sir");
-    console.log(response);
-    const d = await response.json();
-    console.log(d);
-    // setCommentData(d.data);
-    commentData[postId] = d.data;
-  };
+  const bearerToken = localStorage.getItem("token");
+
+  useEffect(() => {
+    GetData();
+  }, []);
 
   const GetData = async () => {
-    const response = await fetch(api, {
-      headers: {
-        projectID: "f104bi07c490",
-      },
-    });
-    const r = await response.json();
-    console.log(r);
-    setData(r["data"]);
-    console.log(Data);
-
-    var delayInMilliseconds = 5000; //1 second
-
-    setTimeout(function () {
-      Data.forEach((i) => {
-        console.log(i._id);
-        getComment(i._id);
-      });
-      //your code to be executed after 1 second
-    }, delayInMilliseconds);
-  };
-
-  ////like count//
-  console.log(Data);
-  Data.forEach((i) => {
-    console.log(i._id);
-    getComment(i._id);
-  });
-  const likePost = async (postId) => {
-    // const token = getBearerToken();
-    const token = localStorage.getItem("token");
-    const token_ = "Bearer " + token;
-    console.log(token_);
-
-    console.log(postId);
-    const response = await fetch(
-      `https://academics.newtonschool.co/api/v1/facebook/like/${postId}`,
-      {
-        method: "POST",
+    try {
+      const response = await fetch("https://academics.newtonschool.co/api/v1/facebook/post", {
         headers: {
-          Authorization: token_,
           projectID: "f104bi07c490",
         },
-      }
-    );
-    console.log(response);
-    const d = await response.json();
-    console.log(d);
-    if (response.ok) {
-      setPostLiked(true);
-    }
-  };
-
-  //updating comment//
-
-  const createCommentForPost = async (postId, content) => {
-    try {
-      const response = await fetch(
-        `https://academics.newtonschool.co/api/v1/facebook/comment/${postId}`,
-        {
-          method: "POST",
-          headers: {
-            Authorization: `Bearer ${bearerToken}`,
-            projectID: "f104bi07c490",
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ content }),
-        }
-      );
-
+      });
       if (response.ok) {
-        console.log("Comment created successfully");
         const data = await response.json();
-        setComments([...comments, data.comment]);
+        setData(data.data);
+
+        data.data.forEach((post) => {
+          // Fetch comments for each post when the component loads
+          handleFetchComments(post._id);
+        });
       } else {
-        const errorData = await response.json();
-        console.error("Error while creating a comment:", errorData);
+        console.error("Error while fetching data.");
       }
     } catch (error) {
       console.error("Error:", error);
     }
   };
-
-  const handleComment = (e) => {
-    setComments(e.target.value);
-  };
-
-  //
-
-  ///fecting comments
 
   const handleFetchComments = async (postId) => {
     try {
@@ -160,10 +63,11 @@ function Homepage() {
         }
       );
       if (response.ok) {
-        console.log("Comment is click");
         const data = await response.json();
-        setComments(data.comments);
-        console.log(data);
+        setComments((prevComments) => ({
+          ...prevComments,
+          [postId]: data.data,
+        }));
       } else {
         const errorData = await response.json();
         console.error("Error while fetching comments:", errorData);
@@ -173,13 +77,11 @@ function Homepage() {
     }
   };
 
-  ////////////////////////
-
   return (
     <div className="post-box">
       {Data &&
         Data.map((post) => (
-          <Card sx={{ maxWidth: 450, maxHeight: 800, height: "50em" }}>
+          <Card sx={{ maxWidth: 450, maxHeight: 800, height: "50em" }} key={post._id}>
             <CardHeader
               avatar={
                 <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
@@ -228,7 +130,6 @@ function Homepage() {
                   <img
                     src={like2}
                     alt="..."
-                    onClick={() => likePost(post._id)}
                     style={{
                       cursor: "pointer",
                       height: "21px",
@@ -246,56 +147,17 @@ function Homepage() {
                 <img
                   src={comment}
                   alt="..."
-                  // onClick={() => getComment(post._id)}
-                  onClick={() => handleFetchComments(post._id)}
                 />
                 <span id="S-comment">Comment</span>
               </div>
             </div>
             <div className="line2"></div>
             <div className="chat-container">
-              <div className="C-input-container">
-                <div
-                  className="input-items"
-                  style={{ display: "flex", alignItems: "center" }}
-                >
-                  <Avatar
-                    style={{
-                      height: "35px",
-                      width: "35px",
-                      marginLeft: "12px",
-                      marginRight: "4px",
-                    }}
-                  />
-                  <input
-                    type="text"
-                    class="text-field-input"
-                    placeholder="Write a public comment..."
-                    value={comments}
-                    onChange={handleComment}
-                    style={{ border: "none" }}
-                  />
-                  <span>
-                    <div id="send-container">
-                      <img
-                        src={send}
-                        alt="..."
-                        style={{ height: "22px", marginRight: "11px" }}
-                        onClick={() => createCommentForPost(post._id, comments)}
-                      />
-                    </div>
-                  </span>
-                </div>
-              </div>
-
-              <div className="scroll-container">
-                {commentData[post._id] &&
-                  commentData[post._id].map((comment) => (
-                    <div>
-                      <div
-                        className="add-commnet-section"
-                        style={{ display: "flex" }}
-                      >
+              {comments[post._id] && (
+                <div className="scroll-container">
+                  {comments[post._id].map((comment) => (
+                    <div key={comment._id}>
+                      <div className="add-commnet-section" style={{ display: "flex" }}>
                         <Avatar
                           style={{
                             height: "35px",
@@ -307,21 +169,20 @@ function Homepage() {
                         />
                         <div className="added-comment">
                           <p>
-                            <strong style={{ fontSize: "12px" }}>
-                              Harry Potter
-                            </strong>
+                            <strong style={{ fontSize: "12px" }}>{comment.author.name}</strong>
                           </p>
                           <p style={{ fontSize: "15px" }}>{comment.content}</p>
                         </div>
                       </div>
                       <div style={{ display: "flex" }} className="l-r-s">
-                        <p>like</p>
+                        <p>Like</p>
                         <p>Reply</p>
                         <p>Share</p>
                       </div>
                     </div>
                   ))}
-              </div>
+                </div>
+              )}
             </div>
           </Card>
         ))}
